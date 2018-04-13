bddb4b26dab7ee7b5e6c0fd75f29382e081d4736
lujy <821185277@qq.com>
2018-04-13 17:01:13 +0800

    添加项目文件。

---
+++ M160CommandApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace M160CommandApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM8";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(928, 388);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(928, 388);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(920, 362);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "160";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(348, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 53);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "中断键被按下";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "开始键被按下";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始实验";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(920, 362);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 394);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(928, 143);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(928, 143);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(511, 30);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 53);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "回传发光值";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 537);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ M160CommandApp/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace M160CommandApp
+{
+    public partial class Form1 : Form
+    {
+        private byte[] recvBufferIn = new byte[512];
+
+
+        private byte[] recvBuffer = new byte[256];
+
+        //接收实时长度
+        private int recvRealLen = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serialPort1.Open();
+        }
+
+        private static byte[] strToToHexByte(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "FA 1B 60 18 01 45 38 45 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FC";
+            //byte[] byteArray = System.Text.Encoding.Default.GetBytes(str);
+
+            byte[] byteArray = strToToHexByte(str);
+
+            serialPort1.Write(byteArray, 0, byteArray.Length);
+        }
+
+        private void translateBuffer()
+        {
+            bool bstarts = false;
+
+            int realLens = 0;
+
+            for (int i = 0; i < this.recvRealLen; i++)
+            {
+                if (0xFC == this.recvBufferIn[i])
+                {
+                    recvBuffer[realLens] = this.recvBufferIn[i];
+                    realLens++;
+                    bstarts = true;
+                    continue;
+                }
+
+                if (!bstarts)
+                    continue;
+
+                if (0xFB == this.recvBufferIn[i])
+                {
+                    if (0xA0 == this.recvBufferIn[i + 1])
+                    {
+                        recvBuffer[realLens] = 0xFA;
+                        realLens++;
+                        i++;
+                    }
+                    else if (0xA1 == this.recvBufferIn[i + 1])
+                    {
+                        recvBuffer[realLens] = 0xFC;
+                        realLens++;
+                        i++;
+                    }
+                    else if (0xA2 == this.recvBufferIn[i + 1])
+                    {
+                        recvBuffer[realLens] = 0xFB;
+                        realLens++;
+                        i++;
+                    }
+                    else
+                    {
+                        recvBuffer[realLens] = this.recvBufferIn[i];
+                        realLens++;
+                    }
+                }
+                else if (0xFA == this.recvBufferIn[i])
+                {
+                    recvBuffer[realLens] = this.recvBufferIn[i];
+                    bstarts = false;
+                    realLens++;
+                    this.HandleEveryCommand(realLens);
+                    realLens = 0;
+                }
+                else
+                {
+                    recvBuffer[realLens] = this.recvBufferIn[i];
+                    realLens++;
+                }
+            }
+
+            //recvBuffer[realLens++] = 0xFC;
+
+
+            this.recvRealLen = 0;
+        }
+
+        private void HandleEveryCommand(int realLens)
+        {
+            StringBuilder anwserBuilder = new StringBuilder("返回帧: ", 512);
+            for (int i = 0; i < realLens; i++)
+            {
+                anwserBuilder.Append(string.Format("{0} ", recvBuffer[i].ToString("X2")));
+            }
+
+            this.BeginInvoke(new MethodInvoker(() =>
+            {
+                this.textBox1.AppendText(anwserBuilder.ToString()+"\n");
+
+            }));
+
+        }
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                int bytes = this.serialPort1.BytesToRead;
+
+                if (this.recvRealLen >= 256)
+                {
+                    this.recvRealLen = 0;
+                }
+
+                if (bytes > 0)
+                {
+                    this.serialPort1.Read(this.recvBufferIn, this.recvRealLen, bytes);
+
+                    this.recvRealLen += bytes;
+
+                    if (this.recvBufferIn[this.recvRealLen - 1] == 0xFA)
+                    {
+                        this.translateBuffer();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.recvRealLen = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = "FA 08 70 10 01 00 00 FC";
+            //byte[] byteArray = System.Text.Encoding.Default.GetBytes(str);
+
+            byte[] byteArray = strToToHexByte(str);
+
+            serialPort1.Write(byteArray, 0, byteArray.Length);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = "FA 08 70 10 00 00 00 FC";
+            //byte[] byteArray = System.Text.Encoding.Default.GetBytes(str);
+
+            byte[] byteArray = strToToHexByte(str);
+
+            serialPort1.Write(byteArray, 0, byteArray.Length);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string str = "FA 1B 40 19 01 45 38 45 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FC";
+            //byte[] byteArray = System.Text.Encoding.Default.GetBytes(str);
+
+            byte[] byteArray = strToToHexByte(str);
+
+            serialPort1.Write(byteArray, 0, byteArray.Length);
+        }
+    }
+}
